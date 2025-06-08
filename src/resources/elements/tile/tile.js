8ab2ea46d178ebd0a5f44a5ef1fe140708c42461
Esger <esgerj@gmail.com>
2025-06-08 23:28:59 +0200

    fix safari hover/unhover
    will-change: opacity did it

--- src/resources/elements/tile/tile.js
+++ src/resources/elements/tile/tile.js
@@ -11,9 +11,36 @@ export class TileCustomElement {
     constructor(eventAggregator) {
         this._eventAggregator = eventAggregator;
     }
+    
+    attached() {
+        this.hover = false;
+    }
 
     push() {
         this._eventAggregator.publish('push', this.tile);
+        this.unHover()
     }
 
+    doHover() {
+        this.hover = true;
+
+        // if (this.isDisabled) return;
+        // this.unhover();
+        // if (this.y == 0) {
+        //     $(`.tile.x_${this.x}`).addClass('hover down');
+        // };
+        // if (this.y == this.maxPosition) {
+        //     $(`.tile.x_${this.x}`).addClass('hover up');
+        // };
+        // if (this.x == 0) {
+        //     $(`.tile.y_${this.y}`).addClass('hover right');
+        // };
+        // if (this.x == this.maxPosition) {
+        //     $(`.tile.y_${this.y}`).addClass('hover left');
+        // };
+    }
+
+    unHover() {
+        this.hover = false;
+    }
 }
